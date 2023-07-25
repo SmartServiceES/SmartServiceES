130f14820f68d19240510580aab81bb78d9be4b9
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-07-25 20:58:06 -0300

    Adicionando validações de serviço

--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,2 +1,10 @@
 class Servico < ApplicationRecord
+
+  validates :nome, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-Z ]+\z/, message: "Use apenas letras"}
+  validates :descricao, presence: true, length: {in: 1..150}, format: {with: /\A[a-zA-Z ]+\z/, message: "Use apenas letras"}
+  validates_datetime :horario_inicio,on_or_after: -> { DateTime.current }, message: "deve ser igual ou posterior à data e hora atual"
+  validates_datetime :horario_termino,on_or_after: -> { DateTime.current }, message: "deve ser igual ou posterior à data e hora atual"
+  validates :valor, numericality: { greater_than_or_equal_to: 0 }
+
+
 end
