15c056edadd6e6795b24cfe49247ab6bc412c115
Pedro Souza <pedrosport10@gmail.com>
2023-07-26 23:02:21 -0300

    Removendo host railway

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "smartservicees-production.up.railway.app"
+  config.hosts << "smartservice.onrender.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
