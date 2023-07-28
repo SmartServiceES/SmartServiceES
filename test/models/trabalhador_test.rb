ac2efea6af03ba5bda41c58e4daccea4423f1949
Pedro Souza <pedrosport10@gmail.com>
2023-07-27 21:41:22 -0300

    Teste de model trabalhador: Criar trabalhador com dados invalidos

--- test/models/trabalhador_test.rb
+++ test/models/trabalhador_test.rb
@@ -19,4 +19,10 @@ class TrabalhadorTest < ActiveSupport::TestCase
 
     assert_equal 'Pedro Caitano', trabalhador.reload.nome_completo
   end
+
+  test 'Criando trabalhador com dados invalidos' do
+    trabalhador = Trabalhador.new nome_completo: 'Pedro Souza', telefone: '81998436108', data_nascimento: '2023-01-15',
+                                  email: 'pedroshouldz@outlook.com', senha: 'senha123', profissao: 'Pedreiro'
+    assert_not trabalhador.save
+  end
 end
