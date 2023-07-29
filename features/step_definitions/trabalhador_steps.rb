6a86e4ac7cc1f9db40e56c02fa1bd329c069b7e4
Fernando7492 <Emidio8000@gmail.com>
2023-07-29 19:13:11 -0300

    Teste de cenario - Editar

--- features/step_definitions/trabalhador_steps.rb
+++ features/step_definitions/trabalhador_steps.rb
@@ -18,7 +18,43 @@ Given('Eu estou na pagina de registrar trabalhadores') do
     click_button "Create Trabalhador"
   end
   
-  Then('Eu vejo a mensagem de sucesso {string}') do |string|
+  Then('Eu vejo a mensagem de criado com sucesso {string}') do |string|
+    expect(page).to have_content(string)
+  end
+
+# Step para criar um trabalhador no banco de dados
+Given('Eu tenho um trabalhador com o nome {string}, telefone {string}, data de nascimento {string}, endereco {string}, email {string}, senha {string} e profissao {string}') do |nome, telefone, data_nascimento_str, endereco, email, senha, profissao|
+    data_nascimento = Date.parse(data_nascimento_str)
+  
+    Trabalhador.create!(
+      nome_completo: nome,
+      telefone: telefone,
+      data_nascimento: data_nascimento,
+      email: email,
+      senha: senha,
+      profissao: profissao
+    )
+  end
+  
+  # Step para acessar a página de edição do trabalhador
+  Given('Eu estou na pagina de edicao do trabalhador com o nome {string}') do |nome|
+    trabalhador = Trabalhador.find_by(nome_completo: nome)
+    visit edit_trabalhador_path(trabalhador)
+  end
+  
+  # Step para atualizar o telefone e a profissão do trabalhador
+  When('Eu atualizo o telefone para {string} e a profissao para {string}') do |novo_telefone, nova_profissao|
+    fill_in "trabalhador[telefone]", with: novo_telefone
+    fill_in "trabalhador[profissao]", with: nova_profissao
+  end
+  
+  # Step para clicar no botão de atualizar trabalhador
+  When('Eu aperto em {string}') do |string|
+    click_button "Update Trabalhador"
+  end
+  
+  # Step para verificar a mensagem de sucesso após a edição do trabalhador
+  Then('Eu vejo a mensagem de editado com sucesso {string}') do |string|
     expect(page).to have_content(string)
   end
   
--- features/trabalhador.feature
+++ features/trabalhador.feature
@@ -7,4 +7,11 @@ Scenario: Registrar prestador de servico
     Given Eu estou na pagina de registrar trabalhadores
     When Eu preencho o nome "Joao Henrique", o telefone "87123456789", a data de nascimento "1990-01-01", o endereco "Rua X", o email "joao@example.com", a senha "123456" e a profissao "Engenheiro"
     And Eu clico em 'Create trabalhador'
-    Then Eu vejo a mensagem de sucesso 'Trabalhador was successfully created.'
+    Then Eu vejo a mensagem de criado com sucesso 'Trabalhador was successfully created.'
+
+Scenario: Editar prestador de servico
+  Given Eu tenho um trabalhador com o nome "Joao Henrique", telefone "12345678998", data de nascimento "1990-01-01", endereco "Rua X", email "joao@example.com", senha "123456" e profissao "Engenheiro"
+  And Eu estou na pagina de edicao do trabalhador com o nome "Joao Henrique"
+  When Eu atualizo o telefone para "98765432132" e a profissao para "Arquiteto"
+  And Eu aperto em 'Update Trabalhador'
+  Then Eu vejo a mensagem de editado com sucesso 'Trabalhador was successfully updated.'
