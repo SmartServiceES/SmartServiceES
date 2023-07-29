42deca5ed3d90dd8441b7878d49675835b255753
Fernando7492 <Emidio8000@gmail.com>
2023-07-29 20:40:52 -0300

    Teste de Cenário - telefone curto

--- features/step_definitions/trabalhador_steps.rb
+++ features/step_definitions/trabalhador_steps.rb
@@ -5,7 +5,7 @@ Given('Eu estou na pagina de registrar trabalhadores') do
     expect(current_path).to eq('/trabalhadors/new')
   end
   
-  When('Eu preencho o nome {string}, o telefone {string}, a data de nascimento {string}, o endereco {string}, o email {string}, a senha {string} e a profissao {string}') do |nome, telefone, data_nascimento_str, endereco, email, senha, profissao|
+  When('Eu preencho nome {string}, o telefone {string}, a data de nascimento {string}, o email {string}, a senha {string} e a profissao {string}') do |nome, telefone, data_nascimento_str, email, senha, profissao|
     data_nascimento = Date.parse(data_nascimento_str)
   
     fill_in "trabalhador[nome_completo]",	with: nome
@@ -26,7 +26,7 @@ Given('Eu estou na pagina de registrar trabalhadores') do
 
   #EDITAR
 
-Given('Eu tenho um trabalhador com o nome {string}, telefone {string}, data de nascimento {string}, endereco {string}, email {string}, senha {string} e profissao {string}') do |nome, telefone, data_nascimento_str, endereco, email, senha, profissao|
+Given('Eu tenho um trabalhador com o nome {string}, telefone {string}, data de nascimento {string}, email {string}, senha {string} e profissao {string}') do |nome, telefone, data_nascimento_str, email, senha, profissao|
     data_nascimento = Date.parse(data_nascimento_str)
   
     Trabalhador.create!(
@@ -58,7 +58,7 @@ Given('Eu tenho um trabalhador com o nome {string}, telefone {string}, data de n
   end
   
   #DELETAR
-Given('Eu tenho um trabalhador com o nome {string}, telefone {string}, data de nascimento {string}, email {string}, senha {string} e profissao {string}') do |nome, telefone, data_nascimento_str, email, senha, profissao|
+Given('Eu tenho um trabalhador com nome {string}, telefone {string}, data de nascimento {string}, email {string}, senha {string} e profissao {string}') do |nome, telefone, data_nascimento_str, email, senha, profissao|
     data_nascimento = Date.parse(data_nascimento_str)
   
     Trabalhador.create!(
@@ -83,4 +83,29 @@ Given('Eu tenho um trabalhador com o nome {string}, telefone {string}, data de n
   Then('Eu vejo a mensagem de sucesso {string}') do |string|
     expect(page).to have_content(string)
   end
+
+#Telefone pequeno
+Given('Eu estou em registrar trabalhadores') do
+    visit '/trabalhadors/new'
+    expect(current_path).to eq('/trabalhadors/new')
+  end
   
+  When('Eu preencho o nome {string}, o telefone {string}, a data de nascimento {string}, o email {string}, a senha {string} e a profissao {string}') do |nome, telefone, data_nascimento_str, email, senha, profissao|
+    data_nascimento = Date.parse(data_nascimento_str)
+  
+    fill_in "trabalhador[nome_completo]", with: nome
+    fill_in "trabalhador[telefone]", with: telefone
+    fill_in "trabalhador[data_nascimento]", with: data_nascimento.strftime('%Y-%m-%d')
+    fill_in "trabalhador[email]", with: email
+    fill_in "trabalhador[senha]", with: senha
+    fill_in "trabalhador[profissao]", with: profissao
+  end
+  
+  When('Eu preciono {string}') do |string|
+    click_button "Create Trabalhador"
+  end
+  
+  Then('Eu vejo a mensagem de erro {string}') do |string|
+    expect(page).to have_content(string)
+  end
+
--- features/trabalhador.feature
+++ features/trabalhador.feature
@@ -5,19 +5,27 @@ Feature: Gerenciamento de prestador de servico
 
 Scenario: Registrar prestador de servico   
     Given Eu estou na pagina de registrar trabalhadores
-    When Eu preencho o nome "Joao Henrique", o telefone "87123456789", a data de nascimento "1990-01-01", o endereco "Rua X", o email "joao@example.com", a senha "123456" e a profissao "Engenheiro"
+    When Eu preencho nome "Joao Henrique", o telefone "87123456789", a data de nascimento "1990-01-01", o email "joao@example.com", a senha "123456" e a profissao "Engenheiro"
     And Eu clico em 'Create trabalhador'
     Then Eu vejo a mensagem de criado com sucesso 'Trabalhador was successfully created.'
 
 Scenario: Editar prestador de servico
-  Given Eu tenho um trabalhador com o nome "Joao Henrique", telefone "12345678998", data de nascimento "1990-01-01", endereco "Rua X", email "joao@example.com", senha "123456" e profissao "Engenheiro"
+  Given Eu tenho um trabalhador com o nome "Joao Henrique", telefone "12345678998", data de nascimento "1990-01-01", email "joao@example.com", senha "123456" e profissao "Engenheiro"
   And Eu estou na pagina de edicao do trabalhador com o nome "Joao Henrique"
   When Eu atualizo o telefone para "98765432132" e a profissao para "Arquiteto"
   And Eu aperto em 'Update Trabalhador'
   Then Eu vejo a mensagem de editado com sucesso 'Trabalhador was successfully updated.'
 
 Scenario: Excluir prestador de servico
-  Given Eu tenho um trabalhador com o nome "Joao Henrique", telefone "87123456789", data de nascimento "1990-01-01", email "joao@example.com", senha "123456" e profissao "Engenheiro"
+  Given Eu tenho um trabalhador com nome "Joao Henrique", telefone "87123456789", data de nascimento "1990-01-01", email "joao@example.com", senha "123456" e profissao "Engenheiro"
   And Eu estou na pagina de detalhes do trabalhador com o nome "Joao Henrique"
   When Eu seleciono a opcao 'Destroy this trabalhador'
   Then Eu vejo a mensagem de sucesso 'Trabalhador was successfully destroyed.'
+
+Scenario: Tentativa de cadastro com número de telefone muito curto
+  Given Eu estou em registrar trabalhadores
+  When Eu preencho o nome "Joao Henrique", o telefone "123", a data de nascimento "1990-01-01", o email "joao@example.com", a senha "123456" e a profissao "Engenheiro"
+  And Eu preciono 'Create Trabalhador'
+  Then Eu vejo a mensagem de erro "Telefone precisa ter 11 dígitos"
+
+
