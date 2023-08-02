3f5ebe87456c51523d2f77408d264f87540b1f5f
Giudicelli <giudicellielias@gmail.com>
2023-08-02 15:54:50 -0300

    Adicionando testes de servicos

--- features/step_definitions/servico_steps.rb
+++ features/step_definitions/servico_steps.rb
@@ -1,117 +1,82 @@
-Given("que existem serviços cadastrados na categoria {string}") do |categoria|
-  @servicos_pedreiro =  Servico.create([
-                                         { nome: 'Encanador 1', categoria: 'encanador' },
-                                         { nome: 'Encanador 2', categoria: 'encanador' },
-                                         { nome: 'pedreiro 1', categoria: 'pedreiro' },
-                                         { nome: 'pedreiro 2', categoria: 'pedreiro' },
-                                         { nome: 'Eletricista 1', categoria: 'eletricista' },
-                                         { nome: 'Eletricista 2', categoria: 'eletricista' }
-                                       ])
+#Criar Servico
 
+Given('Eu estou na pagina de registrar servicos') do
+  visit '/servicos/new'
+  expect(page).to have_current_path('/servicos/new')
 end
 
-When("eu procuro pelos serviços da categoria {string}") do |categoria|
-  @resultados = Servico.where(categoria: categoria)
-end
-
-Then("eu visualizo uma lista de serviços disponíveis de pedreiro") do
-  expect(@resultados.all? { |servico| servico.categoria == 'pedreiro' }).to be true
-end
-
-
-
-
-Given("que contem serviços cadastrados na categoria {string}") do |categoria|
-  @servicos_eletricista = Servico.create([
-                                           { nome: 'Encanador 1', categoria: 'encanador' },
-                                           { nome: 'Encanador 2', categoria: 'encanador' },
-                                           { nome: 'pedreiro 1', categoria: 'pedreiro' },
-                                           { nome: 'pedreiro 2', categoria: 'pedreiro' },
-                                           { nome: 'Eletricista 1', categoria: 'eletricista' },
-                                           { nome: 'Eletricista 2', categoria: 'eletricista' }
-                                         ])
-end
-
-When("procuro serviços nesta categoria {string}") do |categoria|
-  @resultados = Servico.where(categoria: categoria)
-end
-
-Then("eu visualizo uma lista de serviços disponíveis de eletricista") do
-  expect(@resultados.all? { |servico| servico.categoria == 'eletricista' }).to be true
+Given('O trabalhador com nome_completo {string}, telefone {string}, data_de_nascimento {string}, email {string}, senha {string}, profissao {string} existe') do |nome, telefone, data_nascimento_str, email, senha, profissao|
+  visit '/trabalhadors/new'
+  data_nascimento = Date.parse(data_nascimento_str)
+  fill_in "trabalhador[nome_completo]",	with: nome
+  fill_in "trabalhador[telefone]",	with: telefone
+  fill_in "trabalhador[data_nascimento]",	with: data_nascimento.strftime('%Y-%m-%d')
+  fill_in "trabalhador[email]",	with: email
+  fill_in "trabalhador[senha]",	with: senha
+  fill_in "trabalhador[profissao]",	with: profissao
+  click_button 'Create Trabalhador'
+  expect(page).to have_content("Trabalhador was successfully created.")
 end
 
 
+When('Agora preencho os campos de Nome {string} Descricao {string} Valor {string} Categoria {string} Horario inicio {string} Horario termino {string}') do |nome, descricao, valor, categoria, horario_inicio, horario_termino|
+  visit '/servicos/new'
 
-
-Given("que tem serviços cadastrados na categoria {string}") do |categoria|
-  @servicos_encanador = Servico.create([
-                                         { nome: 'Encanador 1', categoria: 'encanador' },
-                                         { nome: 'Encanador 2', categoria: 'encanador' },
-                                         { nome: 'pedreiro 1', categoria: 'pedreiro' },
-                                         { nome: 'pedreiro 2', categoria: 'pedreiro' },
-                                         { nome: 'Eletricista 1', categoria: 'eletricista' },
-                                         { nome: 'Eletricista 2', categoria: 'eletricista' }
-                                       ])
+  fill_in "servico[nome]",	with: nome
+  fill_in "servico[descricao]",	with: descricao
+  fill_in "servico[valor]", with: valor
+  fill_in "servico[categoria]", with: categoria
+  fill_in "servico[horario_inicio]",	with: horario_inicio
+  fill_in "servico[horario_termino]",	with: horario_termino
 end
 
-When("eu busco por um tipo de serviço da categoria {string}") do |categoria|
-  @resultados = Servico.where(categoria: categoria)
+When('Eu clico em Create Servico') do
+  click_button "Create Servico"
 end
 
-Then("eu visualizo uma lista de serviços disponíveis de encanador") do
-  expect(@resultados.all? { |servico| servico.categoria == 'eletricista' }).to be true
-end
-
-
-
-
-Given("que não existem serviços cadastrados na categoria {string}") do |categoria|
-  Servico.where(categoria: categoria)#.destroy_all
-end
-
-When("eu varro pelos serviços da categoria {string}") do |categoria|
-  @servicos_encontrados = Servico.where(categoria: categoria)
-end
-
-Then("eu visualizo uma mensagem informando que não há serviços disponíveis nesta categoria") do
-  if @servicos_encontrados.empty?
-    puts "Não há serviços disponíveis nesta categoria."
-  else
-    puts "A lista de serviços encontrados é:"
-    @servicos_encontrados.each do |servico|
-      puts servico.nome
-    end
-  end
+Then('Vejo {string}') do |mensagem|
+  expect(page).to have_content(mensagem)
 end
 
 
-
-
-
-
-Given("que existem serviços cadastrados nas categorias {string}") do |categorias|
-  categorias_list = categorias.split(', ')
-  @servicos = {}
-  categorias_list.each do |categoria|
-    @servicos[categoria] = Servico.create([
-                                            { nome: 'Encanador 1', categoria: 'encanador' },
-                                            { nome: 'Encanador 2', categoria: 'encanador' },
-                                            { nome: 'pedreiro 1', categoria: 'pedreiro' },
-                                            { nome: 'pedreiro 2', categoria: 'pedreiro' },
-                                            { nome: 'Eletricista 1', categoria: 'eletricista' },
-                                            { nome: 'Eletricista 2', categoria: 'eletricista' }
-                                          ])
-  end
+Then('Vejo a mensagem {string} que o servico não foi criado') do |mensagem|
+  expect(page).to have_content(mensagem)
 end
 
-When("eu vasculo pelos serviços das categorias {string}") do |categorias|
-  categorias_list = categorias.split(', ')
-  @resultados = {}
-  categorias_list.each do |categoria|
-    @resultados[categoria] = Servico.where(categoria: categoria)
-  end
+# Destruir servico
+
+Given('O servico de Nome {string} Descricao {string} Valor {string} Categoria {string} Horario inicio {string} Horario termino {string} existe') do |nome, descricao, valor, categoria, horario_inicio, horario_termino|
+  visit '/servicos/new'
+
+  fill_in "servico[nome]",	with: nome
+  fill_in "servico[descricao]",	with: descricao
+  fill_in "servico[valor]", with: valor
+  fill_in "servico[categoria]", with: categoria
+  fill_in "servico[horario_inicio]",	with: horario_inicio
+  fill_in "servico[horario_termino]",	with: horario_termino
+  click_button "Create Servico"
 end
 
-Then("eu visualizo uma lista de serviços disponíveis para cada categoria") do
-  expect(@resultados.all? ).to be true
+When('Eu clico em servico de nome {string}') do |nome|
+  expect(page).to have_content(nome)
+
+end
+
+When('Eu clico em Destroy this servico') do
+  click_button "Destroy this servico"
+end
+
+#Deletar servico não existente
+
+Given('Eu estou na pagina de servicos') do
+  visit '/servicos'
+  expect(page).to have_current_path('/servicos')
+end
+
+When('Agora na pagina de servico não encontrou servico de Nome {string}') do |string|
+  expect(page).to have_no_content(string)
+end
+
+Then('Continuo na pagina de servico') do
+  expect(page).to have_current_path('/servicos')
 end
