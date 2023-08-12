b5800f0e092b3fb6ce525672c9719e4f22eea65b
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-08-12 01:36:38 -0300

    Correção Test Cliente devido ao Actions

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -5,29 +5,39 @@ class ClienteTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    endereco = Endereco.new(nome_da_rua: 'Rua Santos Dumount',
+    @endereco = Endereco.new(nome_da_rua: 'Rua Santos Dumount',
                             numero_da_casa: '245',
                             cep: '55715000',
                             bairro: 'Jardim Santa Rosa',
                             cidade: 'Feira Nova',
                             complemento: 'Casa')
 
-    @cliente = Cliente.new(nome_completo:'Mauricio Lopes',
+
+  end
+
+  test 'salvar cliente com dados validos' do
+    cliente = Cliente.new(nome_completo:'Mauricio Lopes',
                            data_nascimento: '2000-09-14 00:00',
                            cpf: '05621812450',
                            email: 'mauricio@gmail.com',
                            senha: '11554896',
                            telefone: '87987562145',
-                           endereco: endereco)
-  end
+                           endereco: @endereco)
 
-  test 'salvar cliente com dados validos' do
-    assert @cliente.save
+    assert cliente.save
   end
 
   test 'salvar cliente com telefone incorreto' do
-    @cliente.telefone = '8722113344'
-    assert_not @cliente.save
+
+    cliente = Cliente.new(nome_completo:'Mauricio Lopes',
+                          data_nascimento: '2000-09-14 00:00',
+                          cpf: '05621812450',
+                          email: 'mauricio@gmail.com',
+                          senha: '11554896',
+                          telefone: '8722113344',
+                          endereco: @endereco)
+
+    assert_not cliente.save
   end
 
   test 'salvar cliente com dados invalidos' do
