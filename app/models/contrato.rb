a7e079da82f88b6596bd4064cacd06546367c445
Pedro Souza <pedrosport10@gmail.com>
2023-08-12 18:23:59 -0300

    Front-end: Correção de texto e deletar serviço ao se encerrar contrato

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,6 +1,6 @@
 class Contrato < ApplicationRecord
     belongs_to :cliente
-    belongs_to :servico
+    belongs_to :servico, dependent: :destroy
 
     validates :cliente_id, presence: true
     validates :servico_id, presence: true
