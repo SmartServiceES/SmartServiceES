deee23810a0719572c303255d4b00ddfc8ef0717
Fernando7492 <Emidio8000@gmail.com>
2023-08-11 22:29:38 -0300

    Teste de model contrato - Criando contrato valido

--- test/models/contrato_test.rb
+++ test/models/contrato_test.rb
@@ -4,4 +4,35 @@ class ContratoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @endereco = Endereco.new(nome_da_rua: 'Rua Oliveira',
+                            numero_da_casa: '753',
+                            cep: '55715200',
+                            bairro: 'Prado aguas claras',
+                            cidade: 'Feira Velha',
+                            complemento: 'Apartamento')
+
+    @cliente = Cliente.new(nome_completo:'Vanderson Vilesclaudio',
+                            data_nascimento: '1965-06-12 00:00',
+                            cpf: '05621298450',
+                            email: 'Vanderson@email.com',
+                            senha: '11556547',
+                            telefone: '87986250145',
+                            endereco: endereco)
+
+    @trabalhador = Trabalhador.new( nome_completo: 'Miles Morales', 
+                                    telefone: '81974139856', 
+                                    data_nascimento: '2000-09-14',
+                                    email: 'Miles@email.com', 
+                                    senha: 'senha123', 
+                                    profissao: 'Engenheiro')
+  end
+
+  test 'Criando contrato com dados válidos' do
+    @contrato = Contrato.new(   cliente: @cliente,
+                                trabalhador: @trabalhador)
+    assert @contrato.save
+  end
+
+
 end
