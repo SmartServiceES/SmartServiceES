2e94d280364e0f79f0acbce4c97e20773036ff90
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-08-15 00:00:13 -0300

    Correção no test de Endereco Model

--- test/models/endereco_test.rb
+++ test/models/endereco_test.rb
@@ -3,42 +3,49 @@ require "test_helper"
 class EnderecoTest < ActiveSupport::TestCase
 
   def setup ## devido a chave estrangeira
-    cliente = Cliente.new(nome_completo:'Robert Freire',
-                          data_nascimento: '2003-10-06 00:00',
-                          cpf: '12345678990',
-                          email: 'robert@gmail.com',
-                          senha: '12345678',
-                          telefone: '8712366589',
-                          endereco: @endereco)
+
+    cliente = Cliente.new(nome_completo:'Mauricio Freire',
+                          data_nascimento: '2001-09-14 00:00',
+                          cpf: '09521812450',
+                          email: 'mauricio152@gmail.com',
+                          senha: '11224896',
+                          telefone: '87995562145')
+
     cliente.save
 
+  end
+
+  test 'Criar endereco com todos dados validos' do
     @endereco = Endereco.new(nome_da_rua: 'Rua das Americas',
                              numero_da_casa: '120',
                              cep: '58549985',
                              bairro: 'Sao Cristovao',
                              cidade: 'Recife',
-                             complemento: 'Casa',
-                             cliente_id: cliente.id )
-  end
+                             complemento: 'Casa')
 
-  test 'Criar endereco com todos dados validos' do
     @endereco.save
   end
 
   test 'Atualizar endereco com dados invalidos' do
-    @endereco.cep = '00'
+    @endereco = Endereco.new(nome_da_rua: 'Rua das Americas',
+                             numero_da_casa: '120',
+                             cep: '00',
+                             bairro: 'Sao Cristovao',
+                             cidade: 'Recife',
+                             complemento: 'Casa')
+
     assert_not @endereco.save
   end
 
   test 'Criar endereco com dados invalidos' do
-    endereco = Endereco.new(nome_da_rua: 'Rua',
+    @endereco = Endereco.new(nome_da_rua: 'Rua',
                             numero_da_casa: '120',
                             cep: '58gi85',
                             bairro: 'Sao Cristovao',
                             cidade: '1522',
                             complemento: 'Casa')
 
-    assert_not endereco.save
+    assert_not @endereco.save
   end
 
 
