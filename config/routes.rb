14e4b0b4a40957edec62c612c531f28c80398af2
Pedro Souza <pedrosport10@gmail.com>
2023-08-17 11:18:47 -0300

    Logout da seção

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/password/edit' => 'devise/passwords#edit'
   end
 
   authenticate :user do
