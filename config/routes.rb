1680dcae72364875f9c992fee7ace10138fb4060
Pedro Souza <pedrosport10@gmail.com>
2023-08-17 10:57:26 -0300

    Configurações iniciais da autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,31 @@
 Rails.application.routes.draw do
-  resources :clientes do
-    resources :enderecos
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  get 'home/index'
-  get 'sobre', to: 'pages#sobre'
-  root 'home#index'
 
-  resources :trabalhadors
-  resources :servicos
-  # config/routes.rb
-  # config/routes.rb
-  resources :contratos do
-    collection do
-      get 'search_clientes', to: 'contratos#search_clientes'
+  authenticate :user do
+    root 'home#index'
+    resources :clientes do
+      resources :enderecos
     end
+    get 'home/index'
+
+    resources :trabalhadors
+    resources :servicos
+    # config/routes.rb
+    # config/routes.rb
+    resources :contratos do
+      collection do
+        get 'search_clientes', to: 'contratos#search_clientes'
+      end
+    end
+
+    get '/buscar_servicos', to: 'servicos#buscar', as: 'buscar_servicos'
+    get '/buscar_clientes', to: 'clientes#buscar', as: 'buscar_clientes'
+    get '/buscar_trabalhadors', to: 'trabalhadors#buscar', as: 'buscar_trabalhadors'
   end
 
-
-  get '/buscar_servicos', to: 'servicos#buscar', as: 'buscar_servicos'
-  get '/buscar_clientes', to: 'clientes#buscar', as: 'buscar_clientes'
-  get '/buscar_trabalhadors', to: 'trabalhadors#buscar', as: 'buscar_trabalhadors'
+  get 'sobre', to: 'pages#sobre'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_033741) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_024355) do
   create_table "clientes", force: :cascade do |t|
     t.text "nome_completo"
     t.date "data_nascimento"
@@ -67,6 +67,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_033741) do
     t.string "profissao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "contratos", "clientes"
