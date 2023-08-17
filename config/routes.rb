a16b326a82ce23ba13d770e9141dfc4bb46ecc99
Pedro Souza <pedrosport10@gmail.com>
2023-08-17 12:24:38 -0300

    Melhorando experiencia do usuario no login

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   end
 
   get 'sobre', to: 'pages#sobre'
+  get 'conta', to: 'pages#conta'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
